2b0eb69cd7627fb9654f2d639a3a200fdd466f5c
devamitbera <dev.amitbera@gmail.com>
2015-06-06 10:53:02 +0530

    70064

---
+++ 70064/app/code/local/Magento/Stackexchange70064/Model/Observer.php
@@ -0,0 +1,23 @@
+<?php
+class Magento_Stackexchange70064_Model_Observer{
+	
+	public function addCustomerLoadLayout($observer)
+	{
+		$loggedIn = Mage::getSingleton('customer/session')->isLoggedIn();
+		// add Handler when customer is loggedin
+		if($loggedIn):
+	
+		$groupId = Mage::getSingleton('customer/session')->getCustomerGroupId();
+		//Get customer Group name
+		
+		$group = Mage::getModel('customer/group')->load($groupId);
+		Mage::log('sss'.$group->getName(),null,'am.log');
+		
+		// add handler to current layout
+		$observer->getEvent()->getLayout()->getUpdate()
+		->addHandle('customer_group_'.strtolower($group->getData('customer_group_code')));
+		//Mage::log('customer_group_'.strtolower($group->getData('customer_group_code')), null, 'logfile.log');
+		endif;
+	
+	}
+}
